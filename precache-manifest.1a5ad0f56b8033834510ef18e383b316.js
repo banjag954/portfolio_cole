39f26f85bc140b849b10e5433874e99317419895
ColeKim <banjag954@naver.com>
2020-02-16 19:27:11 +0900

    Updates

---
+++ precache-manifest.1a5ad0f56b8033834510ef18e383b316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f783d9f66a3e6219a1253b71aac2c2e1",
+    "url": "/portfolio_cole/index.html"
+  },
+  {
+    "revision": "57ca7667e49680426023",
+    "url": "/portfolio_cole/static/css/main.9c49e471.chunk.css"
+  },
+  {
+    "revision": "becb9e80e55e64ea753f",
+    "url": "/portfolio_cole/static/js/2.77f77c77.chunk.js"
+  },
+  {
+    "revision": "7eabd6faac284efa210c510555c2674d",
+    "url": "/portfolio_cole/static/js/2.77f77c77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57ca7667e49680426023",
+    "url": "/portfolio_cole/static/js/main.59505e3c.chunk.js"
+  },
+  {
+    "revision": "bc75eb0ac37d35b07473",
+    "url": "/portfolio_cole/static/js/runtime-main.cab5c4df.js"
+  }
+]);
