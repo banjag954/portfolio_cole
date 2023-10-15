16a62468a395e0e29da8c0b625a45dc4cbbf0dc4
banjag954 <56146624+Archisketch-Cole@users.noreply.github.com>
2023-10-15 14:51:22 +0900

    fix: change introduction infos

---
+++ build/precache-manifest.1974644350af2eb5460047f0e383b1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d240efa90997a65ead0d",
+    "url": "/static/css/main.684d5087.chunk.css"
+  },
+  {
+    "revision": "d240efa90997a65ead0d",
+    "url": "/static/js/main.e983e30d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac858c94688021ea4a0c",
+    "url": "/static/js/2.05f9dbae.chunk.js"
+  },
+  {
+    "revision": "e0c3b8cd3486ca71e771dfe1ea69a710",
+    "url": "/index.html"
+  }
+];
