7ab1b0955601efb834500605cdd73366ab883af1
banjag954 <56146624+Archisketch-Cole@users.noreply.github.com>
2023-10-15 19:41:20 +0900

    fix: change introduction infos_02

---
+++ build/precache-manifest.8a0066484e8c3d9d76244f539c11059d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aae9b24a180220946cd",
+    "url": "/static/css/main.684d5087.chunk.css"
+  },
+  {
+    "revision": "6aae9b24a180220946cd",
+    "url": "/static/js/main.b4f27d13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ef9e16a30fd49e73b66",
+    "url": "/static/js/2.0b445134.chunk.js"
+  },
+  {
+    "revision": "b6bfeee8c50a204719464bbbfc081962",
+    "url": "/index.html"
+  }
+];
